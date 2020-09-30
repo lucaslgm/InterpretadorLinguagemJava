1ec223bee50cd6b45916bc9308f17b0e638ef620
Lucas Martins <l.martins90@edu.pucrs.br>
2020-09-29 21:35:32 -0300

    Criação das condicionais que não foram feitas no commit inicial

--- src/Source.java
+++ src/Source.java
@@ -1,48 +1,68 @@
+import java.util.Scanner;
+
 public class Source {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         int[] memoria = new int[1000];
+        char[] toPrint = new char[1000];
         int indice = 0;
+        int count = 0;
 
         for(int posicao : memoria){
             posicao = 0;
         }
+        //TODO: Ver como fazer pra receber as infos
+        System.out.println("input: ");
+        String aux = sc.next();
 
-        char aux = ' ';
-
-        do {
-            if(aux == '>'){
+        for(int i = 0; i < aux.length(); i++){
+            if(aux.charAt(i) == '>'){
                 indice++;
             }
-            else if(aux == '<'){
+            else if(aux.charAt(i) == '<'){
                 indice--;
             }
-            else if(aux == '+'){
+            else if(aux.charAt(i) == '+'){
                 memoria[indice]++;
             }
-            else if(aux == '-'){
+            else if(aux.charAt(i) == '-'){
                 memoria[indice]--;
             }
-            else if(aux == '['){
+            else if(aux.charAt(i) == '['){
                 if(memoria[indice] == 0) {
-                    //TODO
+                    do{
+                        indice++;
+                    }
+                    while(memoria[indice] != ']');
                 }
-                else{
-                    indice++;
-                }
+                indice++;
             }
-            else if(aux == ']'){
+            else if(aux.charAt(i) == ']'){
                 if(memoria[indice] != 0) {
-                    //TODO
+                    do{
+                        indice--;
+                    }
+                    while(memoria[indice] != '[');
                 }
+                indice--;
             }
-            else if(aux == ','){
+            else if(aux.charAt(i) == ','){
                 //TODO: ler entrada do arquivo IF
-                memoria[indice] = -1; //TODO: Arquivo lido
+                System.out.println("input: ");
+                memoria[indice] = sc.nextInt(); //TODO: Arquivo lido
             }
-            else if(aux == '.'){
-                //TODO: escrever no arquivo OF de memoria[indice]
+            else if(aux.charAt(i) == '.'){
+                //TODO: escrever no arquivo OF
+                toPrint[count] = (char) memoria[indice];
+                count++;
+            }
+            else if(aux.charAt(i) == '.'){
+                for(char c : toPrint)
+                {
+                    System.out.println(c);
+                }
+                break;
             }
         }
-        while (aux != '$');
     }
 }
