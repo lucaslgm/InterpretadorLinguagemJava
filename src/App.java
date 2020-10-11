8ccc4a85caecd7accc55e82f4917b32e42ea0adc
Vitor Delela <vitordelela@MacBook-Air-de-Fernanda.local>
2020-10-11 18:22:57 -0300

    last updates

--- src/App.java
+++ src/App.java
@@ -4,17 +4,14 @@ import java.util.List;
 public class App {
     public static void main(String[] args){
         WriterReader.setDir();
-        //WriterReader.fileWriter("","InputFile", false);
         WriterReader.fileWriter("","OutputFile", false);
 
         List<String> sourceFile = WriterReader.inputReader("./arquivos/SourceFile.txt");
         List<String> inputFile = new ArrayList<>();
 
         for (String line : sourceFile){
-//            line = line.replaceAll("[^-><+\\]\\[,.$]","");
             if(line.charAt(0) != '%'){
                 for (int i = 0; i < line.length(); i++){
-                    //WriterReader.fileWriter(Character.toString(line.charAt(i)),"InputFile", true);
                     inputFile.add(Character.toString(line.charAt(i)));
                 }
             }
